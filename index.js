05da52d867b600f953ccddbc82e388e2063be07f
gonzalo rojas <rojasgonzalo23@gmail.com>
2022-09-26 17:18:05 -0300

    config

---
+++ index.js
@@ -0,0 +1,46 @@
+const mongoose = require ('mongoose');
+const routes = require("./routes");
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv').config({path: 'variables.env'})
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true
+});
+ 
+// crear el servidor
+const app = express();
+
+// habilitar bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// importar cors permite que un cliente se conecte a otro servidor
+//definir dominios para recibir peticiones
+const whitelist = [process.env.FRONTEND_URL]
+const corsOptions = {
+    origin: (origin, callback) => {
+        const existe = whitelist.some(dominio => dominio === origin);
+        if(existe){
+            callback(null, true)
+        }else{
+            callback(new Error('No permitido por cors'))
+        }
+    }
+}
+//habilitar cors
+app.use(cors(corsOptions));
+
+// Rutas de la app
+app.use('/', routes());
+
+// carpeta publica
+
+// puerto
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, ()=>{
+    console.log('el servidor esta funcionando');
+});
