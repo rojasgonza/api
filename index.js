f81bd895ef232f63eade7eeafdd477f157b4d611
gonzalo rojas <rojasgonzalo23@gmail.com>
2022-09-27 08:59:13 -0300

    config

--- index.js
+++ index.js
@@ -18,19 +18,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // importar cors permite que un cliente se conecte a otro servidor
 //definir dominios para recibir peticiones
-const whitelist = [process.env.FRONTEND_URL];
-const corsOptions = {
-    origin: (origin, callback) => {
-        console.log(origin)
-        const existe = whitelist.some( dominio => dominio === origin);
-        if(existe){
-            callback(null, true)
-        }else{
-            callback(new Error('No permitido por cors'))
-        }
-    }
-}
-app.use(cors(corsOptions));
+
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+    // Pass to next layer of middleware
+    next();
+ });
+
+
+
+
+
+
+// const whitelist = [process.env.FRONTEND_URL];
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         console.log(origin)
+//         const existe = whitelist.some( dominio => dominio === origin);
+//         if(existe){
+//             callback(null, true)
+//         }else{
+//             callback(new Error('No permitido por cors'))
+//         }
+//     }
+// }
+// app.use(cors(corsOptions));
 
 //habilitar cors
 
